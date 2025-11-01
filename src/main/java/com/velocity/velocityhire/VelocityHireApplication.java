7d16ac5aecd76f9eda7a03e96d9566d973d5ad17
Ahmed Ikram <ikramahmed398@gmail.com>
2025-11-01 08:22:26 +0500

    chore: initialized project and files

---
+++ src/main/java/com/velocity/velocityhire/VelocityHireApplication.java
@@ -0,0 +1,13 @@
+package com.velocity.velocityhire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelocityHireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelocityHireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/velocity/velocityHire/VelocityHireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velocity.velocityHire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelocityHireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

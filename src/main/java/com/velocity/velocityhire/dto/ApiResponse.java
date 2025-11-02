5da549d23b4f81573fc99835bb4f51d15c7e75b1
Ahmed Ikram <ikramahmed398@gmail.com>
2025-11-02 20:46:09 +0500

    feat: added API response

---
+++ src/main/java/com/velocity/velocityhire/dto/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.velocity.velocityhire.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private T data;
+    private String message;
+}
